3ef0c9fdfaf2292e3d0fbec97f8d1fd2075f828b
Daniel Rodriguez <maieutiquer@gmail.com>
2021-11-25 18:03:00 +0200

    enable using docker without cloning the entire repo
    cp -R
    
    fix copy of config
    
    fix permissions for public folder volumes
    
    clean up unused bundled themes for faster install/update
    
    fix var permissions by creating a non-admin user for testing in prod
    
    publish dev image to speed up dev reinstall from scratch
    
    update docker actions
    
    remove context
    
    revert remove context
    
    revert downgrade, remove context
    
    try changing context without changing it
    
    setup buildx
    
    remove dev from labels
    
    change paths
    
    no cache
    
    rename dev action
    
    clean cache
    
    revert clean cache, print pwd and ls
    
    use local cache
    
    use gh cache
    
    cleanup
    
    split dev image and dev build
    
    fix dev build
    
    fix latest dev tag in compose file

--- .docker/web/entrypoint.sh
+++ .docker/web/entrypoint.sh
@@ -2,9 +2,22 @@
 
 set -e
 
+echo "Creating required directories in volumes"
+mkdir -p var/geoip
+mkdir -p files/config
+mkdir -p files/data
+mkdir -p files/templates
+
+echo "Copying initial config files to /config volume"
+cp -R ../initial/config ./
+
+echo "Generating parameters.yml and bundles.ini"
 php bin/configure # we run it again to generate parameters.yml inside the volume
 composer bundles # we run it again to generate bundles.ini inside the volume
 composer delete-cache # fixes install/update errors
+
+echo "Cleaning up unused bundled themes for faster install/update..."
+rm -rf src/main/theme/Resources/themes/claroline-black src/main/theme/Resources/themes/claroline-mint src/main/theme/Resources/themes/claroline-ruby
 
 # Wait for MySQL to respond, depends on mysql-client
 echo "Waiting for $DB_HOST..."
@@ -20,6 +33,7 @@ if [ -f files/installed ]; then
 
   php bin/console claroline:update -vvv
 else
+
   echo "Installing Claroline for the first time..."
   php bin/console claroline:install -vvv
 
@@ -31,6 +45,18 @@ else
   if [[ -v PLATFORM_SUPPORT_EMAIL ]]; then
     echo "Changing platform support email to $PLATFORM_SUPPORT_EMAIL";
     sed -i "/support_email: null/c\support_email: $PLATFORM_SUPPORT_EMAIL" files/config/platform_options.json
+  fi
+  
+  USERS=$(mysql $DB_NAME -u $DB_USER -p$DB_PASSWORD -h $DB_HOST -se "select count(*) from claro_user")
+
+  if [ "$USERS" == "1" ] && [ -v ADMIN_FIRSTNAME ] && [ -v ADMIN_LASTNAME ] && [ -v ADMIN_USERNAME ] && [ -v ADMIN_PASSWORD ]  && [ -v ADMIN_EMAIL ]; then
+    echo '*********************************************************************************************************************'
+    echo "Creating default non-admin user for production : $ADMIN_FIRSTNAME $ADMIN_LASTNAME $ADMIN_USERNAME $ADMIN_PASSWORD $ADMIN_EMAIL"
+    echo '*********************************************************************************************************************'
+
+    php bin/console claroline:user:create $ADMIN_FIRSTNAME $ADMIN_LASTNAME $ADMIN_USERNAME $ADMIN_PASSWORD $ADMIN_EMAIL
+  else
+    echo 'Users already exist or no admin vars detected, Claroline installed without an admin account'
   fi
 
   echo "In order to create an admin user, run the following command inside the docker container (and replace the variables):"
@@ -44,6 +70,7 @@ echo "Clean cache after setting correct permissions, fixes SAML issues"
 composer delete-cache # fixes SAML errors
 
 echo "Setting correct file permissions for PROD"
+chmod -R 755 public/js public/themes
 chmod -R 750 var files config
 chown -R www-data:www-data var files config
 
