face5c046de50a9f3e22c88a5845c7ba2bf9c204
Daniel Rodriguez <maieutiquer@gmail.com>
2022-01-11 18:48:30 +0200

    fix trailing conflict

--- src/plugin/cursus/Serializer/CourseSerializer.php
+++ src/plugin/cursus/Serializer/CourseSerializer.php
@@ -174,16 +174,11 @@ class CourseSerializer
         $this->sipe('meta.tutorRoleName', 'setTutorRoleName', $data, $course);
         $this->sipe('meta.learnerRoleName', 'setLearnerRoleName', $data, $course);
         $this->sipe('meta.icon', 'setIcon', $data, $course);
-<<<<<<< HEAD
         $this->sipe('meta.days', 'setDefaultSessionDays', $data, $course);
         $this->sipe('meta.hours', 'setDefaultSessionHours', $data, $course);
-        $this->sipe('meta.order', 'setOrder', $data, $course);
-=======
-        $this->sipe('meta.duration', 'setDefaultSessionDuration', $data, $course);
 
         $this->sipe('display.order', 'setOrder', $data, $course);
         $this->sipe('display.hideSessions', 'setHideSessions', $data, $course);
->>>>>>> ad7a299996ff24d409e64fe11d5ceaec026e4562
 
         $this->sipe('restrictions.users', 'setMaxUsers', $data, $course);
         $this->sipe('restrictions.hidden', 'setHidden', $data, $course);
