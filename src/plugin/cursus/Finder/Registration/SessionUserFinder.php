5d6faa81b1333598599d45357a8df1ee4f5b3e6b
firepolo <anthony.pasche@gmail.com>
2022-01-31 15:46:23 +0100

    fix display course

--- src/plugin/cursus/Finder/Registration/SessionUserFinder.php
+++ src/plugin/cursus/Finder/Registration/SessionUserFinder.php
@@ -27,8 +27,6 @@ class SessionUserFinder extends AbstractFinder
         $qb->join('obj.user', 'u');
         $qb->join('obj.session', 's');
         $qb->join('s.course', 'c');
-        $userJoin = false;
-        $sessionJoin = false;
 
         foreach ($searches as $filterName => $filterValue) {
             switch ($filterName) {
@@ -64,21 +62,11 @@ class SessionUserFinder extends AbstractFinder
                     break;
 
                 case 'user':
-                    if (!$userJoin) {
-                        $qb->join('obj.user', 'u');
-                        $userJoin = true;
-                    }
-
                     $qb->andWhere("u.uuid = :{$filterName}");
                     $qb->setParameter($filterName, $filterValue);
                     break;
 
                 case 'organizations':
-                    if (!$userJoin) {
-                        $qb->join('obj.user', 'u');
-                        $userJoin = true;
-                    }
-
                     $qb->join('u.organizations', 'o');
                     $qb->andWhere("o.uuid IN (:{$filterName})");
                     $qb->setParameter($filterName, $filterValue);
