e9bcdc4194e0bea9b384b98c8824bb69f8b53d0b
Daniel Rodriguez <dani@plam.ch>
2022-06-06 18:14:31 +0300

    hide menu link to public events

--- src/plugin/cursus/Resources/modules/tools/trainings/components/menu.jsx
+++ src/plugin/cursus/Resources/modules/tools/trainings/components/menu.jsx
@@ -22,11 +22,6 @@ const TrainingsMenu = (props) =>
           label: trans('catalog', {}, 'cursus'),
           target: `${props.path}/catalog`
         }, {
-          name: 'public',
-          type: LINK_BUTTON,
-          label: trans('public_events', {}, 'cursus'),
-          target: props.path + '/events/public'
-        }, {
           name: 'all',
           type: LINK_BUTTON,
           label: trans('all_events', {}, 'cursus'),
