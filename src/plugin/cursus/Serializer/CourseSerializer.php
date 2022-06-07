7590711ba0fcff7c376d1d9b84bfee4a892b1778
daniel rodriguez <maieutiquer@users.noreply.github.com>
2022-06-07 15:27:32 +0300

    Session duration (#93)
    * replace session_duration by session_days, session_hours
    
    * add translation for the labels days, hours
    
    * fix code
    
    * add updater
    
    * add separator between days and hours
    
    * fix code
    
    * fix trailing conflict
    
    Co-authored-by: firepolo <anthony.pasche@gmail.com>

--- src/plugin/cursus/Serializer/CourseSerializer.php
+++ src/plugin/cursus/Serializer/CourseSerializer.php
@@ -114,7 +114,8 @@ class CourseSerializer
                     'updated' => DateNormalizer::normalize($course->getUpdatedAt()),
                     'tutorRoleName' => $course->getTutorRoleName(),
                     'learnerRoleName' => $course->getLearnerRoleName(),
-                    'duration' => $course->getDefaultSessionDuration(),
+                    'days' => $course->getDefaultSessionDays(),
+                    'hours' => $course->getDefaultSessionHours(),
                 ],
                 'display' => [
                     'order' => $course->getOrder(),
@@ -168,7 +169,8 @@ class CourseSerializer
         $this->sipe('meta.tutorRoleName', 'setTutorRoleName', $data, $course);
         $this->sipe('meta.learnerRoleName', 'setLearnerRoleName', $data, $course);
         $this->sipe('meta.icon', 'setIcon', $data, $course);
-        $this->sipe('meta.duration', 'setDefaultSessionDuration', $data, $course);
+        $this->sipe('meta.days', 'setDefaultSessionDays', $data, $course);
+        $this->sipe('meta.hours', 'setDefaultSessionHours', $data, $course);
 
         $this->sipe('display.order', 'setOrder', $data, $course);
         $this->sipe('display.hideSessions', 'setHideSessions', $data, $course);
