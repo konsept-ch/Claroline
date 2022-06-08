a85ae58a37f11b79a548fe064b4691085f1632e1
firepolo <anthony.pasche@gmail.com>
2022-06-08 18:17:09 +0200

    fix php code

--- src/plugin/cursus/Repository/SessionUserRepository.php
+++ src/plugin/cursus/Repository/SessionUserRepository.php
@@ -29,7 +29,7 @@ class SessionUserRepository extends EntityRepository
             ')
             ->setParameters([
                 'type' => AbstractRegistration::LEARNER,
-                'organization' => $organization
+                'organization' => $organization,
             ])
             ->getResult();
     }
