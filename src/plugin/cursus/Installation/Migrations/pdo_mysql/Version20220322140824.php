21098a4738fe42b3e4ddd4f8b505f4b0f2a91b8f
daniel rodriguez <maieutiquer@users.noreply.github.com>
2022-06-20 14:33:26 +0300

    fix php cs (#103)

--- src/plugin/cursus/Installation/Migrations/pdo_mysql/Version20220322140824.php
+++ src/plugin/cursus/Installation/Migrations/pdo_mysql/Version20220322140824.php
@@ -14,17 +14,17 @@ class Version20220322140824 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
-        $this->addSql("
+        $this->addSql('
             ALTER TABLE claro_cursusbundle_course_session_cancellation 
             ADD inscription_uuid VARCHAR(36) NOT NULL
-        ");
+        ');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql("
+        $this->addSql('
             ALTER TABLE claro_cursusbundle_course_session_cancellation 
             DROP inscription_uuid
-        ");
+        ');
     }
 }
