c8b44c7aff8eb45e7e99870d4ba2f9a2af8332b5
daniel rodriguez <maieutiquer@users.noreply.github.com>
2022-09-16 15:40:34 +0300

    Quota years (#134)
    * add options in quota
    
    * update data from old to new format
    
    * fix migrations & managed subscriptions
    
    * add year query param & fix quota form
    
    * add year on controller
    
    * add year anywhere
    
    * fix quota options
    
    * fix update mistakes
    
    * fix db migration
    
    temporary post install tasks
    
    change method for migrate db
    
    migrate data in command
    
    fix command
    
    fix migrate command
    
    fix migrate error
    
    fix remark
    
    * fix php code
    
    * fix php code
    
    * fix eslint
    
    Co-authored-by: firepolo <anthony.pasche@gmail.com>

---
+++ src/plugin/cursus/Resources/modules/data/types/quota/components/input.jsx
@@ -0,0 +1,124 @@
+import React, {Fragment, useState, useEffect} from 'react'
+
+import {FormControl, ControlLabel, Checkbox} from 'react-bootstrap'
+
+import {ContentSection, ContentSections} from '#/main/app/content/components/sections'
+
+import {Button} from '#/main/app/action/components/button'
+import {CALLBACK_BUTTON} from '#/main/app/buttons'
+
+import {trans} from '#/main/app/intl/translation'
+import {PropTypes as T, implementPropTypes} from '#/main/app/prop-types'
+import {DataInput as DataInputTypes} from '#/main/app/data/types/prop-types'
+
+const QuotaInput = props => {
+  const [def, setDef] = useState(props.value.default)
+  const [years, setYears] = useState(props.value.years)
+  const [choices, setChoices] = useState(Array.from({length:31}, (_, i) => 2020 + i).reduce((acc, year) => {
+    return {
+      ...acc,
+      [year]: !years[year]
+    }
+  }, {}))
+  const [choice, setChoice] = useState()
+
+  useEffect(() => setChoice(Object.entries(choices).find(entry => entry[1] && entry[0] != choice)[0]), [choices])
+  useEffect(() => props.onChange({ default: def, years }), [years, def])
+
+  return (
+    <Fragment>
+      <ContentSections>
+        <ContentSection title={trans('default')}>
+          <ControlLabel>{trans('enabled')}</ControlLabel>
+          <Checkbox name="quota_default_enabled" checked={def.enabled} onChange={e => setDef({
+            enabled: e.target.checked,
+            quota: def.quota
+          })} />
+          <ControlLabel>{trans('quota', {}, 'cursus')}</ControlLabel>
+          <FormControl name="quota_default_quota" type="number" value={def.quota} onChange={e => setDef({
+            enabled: def.enabled,
+            quota: e.target.value
+          })} />
+        </ContentSection>
+
+        {Object.entries(years).sort((a, b) => a[0] - b[0]).map(([year, {enabled, quota}]) =>
+          <ContentSection key={year} title={year} actions={[
+            {
+              name: 'remove-year',
+              type: CALLBACK_BUTTON,
+              icon: 'fa fa-fw fa-trash',
+              label: trans('remove_year'),
+              callback: () => {
+                // eslint-disable-next-line no-unused-vars
+                const {[year]: _year, ...restYears} = years
+                setYears(restYears)
+                setChoices({
+                  ...choices,
+                  [year]: true
+                })
+              }
+            }
+          ]}>
+            <ControlLabel>{trans('enabled')}</ControlLabel>
+            <Checkbox name={`quota_${year}_enabled`} checked={enabled} onChange={e => setYears({
+              ...years,
+              [year]: {
+                enabled: e.target.checked,
+                quota
+              }
+            })} />
+            <ControlLabel>{trans('quota', {}, 'cursus')}</ControlLabel>
+            <FormControl name={`quota_${year}_quota`} type="number" value={quota} onChange={e => setYears({
+              ...years,
+              [year]: {
+                enabled,
+                quota: e.target.value
+              }
+            })} />
+          </ContentSection>
+        )}
+      </ContentSections>
+
+      <FormControl componentClass="select" value={choice} onChange={e => setChoice(e.target.value)}>
+        {Object.entries(choices).filter(c => c[1]).map(([year]) =>
+          <option key={year} value={year}>{year}</option>
+        )}
+      </FormControl>
+      <br />
+      <Button
+        type={CALLBACK_BUTTON}
+        className="btn btn-block"
+        icon="fa fa-fw fa-plus"
+        label={trans('add_year', {}, 'cursus')}
+        size={props.size}
+        callback={() => {
+          setYears({
+            ...years,
+            [choice]: def
+          })
+          setChoices({
+            ...choices,
+            [choice]: false
+          })
+        }}
+      />
+    </Fragment>
+  )
+}
+
+implementPropTypes(QuotaInput, DataInputTypes, {
+  value: T.object.isRequired
+}, {
+  value: {
+    default: {
+      enabled: false,
+      quota: 0
+    },
+    years: {
+    }
+  }
+})
+
+export {
+  QuotaInput
+}
